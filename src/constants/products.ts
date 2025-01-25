acdcd2d06699ec0bda0fb9f07f20722450147ede
AndersonVoeja <am@voeja.us>
2025-01-25 20:07:04 -0300

    Add products constant for product listings

---
+++ src/constants/products.ts
@@ -0,0 +1,6 @@
+export const products = [
+  { id: 1, name: "Pass", value: 99.9 },
+  { id: 2, name: "Estacionamento", value: 80 },
+  { id: 3, name: "Excalibur", value: 120 },
+  { id: 4, name: "Fast Pass", value: 99.99 },
+];
