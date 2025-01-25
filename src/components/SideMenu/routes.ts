4b4ee51f2217e834ec2505b593eb49acdc806067
AndersonVoeja <am@voeja.us>
2025-01-25 18:27:53 -0300

    Add TabsMenu for SideMenu navigation with icons

---
+++ src/components/SideMenu/routes.ts
@@ -0,0 +1,16 @@
+import { CalendarIcon, ChartBarIcon, CogIcon } from "lucide-react";
+
+export const TabsMenu = [
+  {
+    name: "Dashboard",
+    icon: ChartBarIcon,
+  },
+  {
+    name: "Metas",
+    icon: CalendarIcon,
+  },
+  {
+    name: "Configurações",
+    icon: CogIcon,
+  },
+];
